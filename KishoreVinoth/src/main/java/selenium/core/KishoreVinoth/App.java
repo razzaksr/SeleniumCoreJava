67fae8f9fe9e1d5abb37dce51f41395503667855
razzaksr <razzaksr@gmail.com>
2021-02-12 15:09:38 +0530

    Intro demo

---
+++ KishoreVinoth/src/main/java/selenium/core/KishoreVinoth/App.java
@@ -0,0 +1,13 @@
+package selenium.core.KishoreVinoth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Zealous Tech corp" );
+    }
+}
