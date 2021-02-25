3ba84e21558cfe9066bef25a0ff239779964b792
razzaksr <razzaksr@gmail.com>
2021-02-25 14:44:05 +0530

    Loop Intro

---
+++ KishoreVinoth/src/main/java/selenium/core/KishoreVinoth/LoopIntro.java
@@ -0,0 +1,49 @@
+package selenium.core.KishoreVinoth;
+
+/*
+ * Looping statements:
+
+repeatation
+to reduce time complexity
+
+start>> initialization
+step up>> iteration
+end>>condition
+
+
+4 types:
+
+//init
+while(cond)
+{
+// repeation statements
+// iter
+}
+
+
+for(init;cond;iter)
+{
+// repeation
+}
+
+//foreach
+for(type local:source	)
+{
+
+}
+
+//init
+do
+{
+//repeation
+//iter
+}while(cond);
+
+
+continue>> skip
+break>> come out
+ */
+
+public class LoopIntro {
+
+}
